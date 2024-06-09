3089cb982f42a0d74de5f23b537de731ac405fab
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-06-09 23:01:10 +0530

    WIP: login page ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return <div className="grid grid-cols-2 h-100">
+        <div className='h-screen bg-cover bg-[url("../../public/images/login-bg.jpg")]'>
+        </div>
+        <div className="h-screen bg-rose-700">
+
+        </div>
+    </div>
+}
