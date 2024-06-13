7f4646d002d07d0886780d35d17330b94f51eb12
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-06-14 02:40:32 +0530

    WIP: login screen ui, Input component modified for accepting icons and styling

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,26 @@
+import InputField from "@/components/common/InputField";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 export default function Page() {
     return <div className="grid grid-cols-2 h-100">
         <div className='h-screen bg-cover bg-[url("../../public/images/login-bg.jpg")]'>
         </div>
-        <div className="h-screen ">
+        <div className="h-screen">
+            <div>
+                <div>
+                    <h1>Login into your account</h1>
+                </div>
+                <div>
+                    <div className="mb-3">
+                        <InputField type="email" placeholder="Your Email Address" Icon={MdOutlineMailOutline} />
+                    </div>
+                    <div className="mb-3">
+                        <InputField type="password" placeholder="Password" Icon={RiLockPasswordLine} />
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     </div>
 }
--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -1,24 +1,32 @@
-import { InputType } from "@/types/constants/INPUT_TYPES";
 import React from "react"
+import classNames from "classnames";
+
+import { InputType } from "@/types/constants/INPUT_TYPES";
+import { IconType } from "react-icons";
 
 interface IInputField {
     label?: string;
     placeholder?: string;
     type: InputType;
-    value: string;
+    value?: string;
+    className?: string;
+    Icon?: IconType
     onChange?: (value: string) => void;
 }
 
-const InputField: React.FC<IInputField> = ({ placeholder, label, type, value }) => {
-    return <>
-        <div>
-            <input
-                placeholder={placeholder}
-                type={type}
-                value={value}
-            />
-        </div>
-    </>
+const InputField: React.FC<IInputField> = ({ placeholder, label, type, value, className, Icon }) => {
+    return <div className="relative">
+        {/* <MdOutlineMailOutline className="absolute top-1/2 translate-x-4 -translate-y-1/2 text-[#999] text-xl"/> */}
+        {Icon && <Icon className="absolute top-1/2 translate-x-4 -translate-y-1/2 text-[#999] text-xl" />}
+        <input
+            className={
+                classNames("w-full border-2 p-3 border-solid border-[#999] rounded text-sm focus:border-primary focus:outline-primary", className, {"pl-12": !!Icon})
+            }
+            placeholder={placeholder}
+            type={type}
+            value={value}
+        />
+    </div>
 }
 
 export default InputField
